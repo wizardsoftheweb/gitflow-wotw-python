6b2c7124fca89fb4dfc06d689055d06ccafddafb
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-11 19:34:34 -0500

    Tweak declaration order

--- gitflow_wotw/components/__init__.py
+++ gitflow_wotw/components/__init__.py
@@ -1,6 +1,6 @@
 # pylint: disable=W,C,R
 
-from .base_action import Action
 from .base_argument import Argument
+from .base_action import Action
 from .base_subcommand import Subcommand
 from .command import Command
