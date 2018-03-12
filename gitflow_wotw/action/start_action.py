9d3284f26b70ed0af05a9d7886c49177b8c02b7e
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-11 19:25:32 -0500

    Update Action location

--- gitflow_wotw/action/start_action.py
+++ gitflow_wotw/action/start_action.py
@@ -2,7 +2,7 @@
 
 from __future__ import print_function
 
-from gitflow_wotw.action import Action
+from gitflow_wotw.components import Action
 from gitflow_wotw.arguments import ShowcommandsArgument
 
 
