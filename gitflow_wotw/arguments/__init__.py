1b8ba352b800e8b99e7ed4f7e64f4da38f6374e5
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-11 19:16:46 -0500

    Export ShowcommandsArgument

--- gitflow_wotw/arguments/__init__.py
+++ gitflow_wotw/arguments/__init__.py
@@ -4,3 +4,4 @@ from .sink_parser import SinkParser
 from .parser import Parser
 from .source_parser import SourceParser
 from .base_argument import Argument
+from .showcommands_argument import ShowcommandsArgument
