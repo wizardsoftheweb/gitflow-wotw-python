d0c8dd6406c1f37bfae30c9512df07a6cd6abd26
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 14:15:21 -0500

    Remove nargs to prevent set from being a list

--- gitflow_wotw/arguments/set_argument.py
+++ gitflow_wotw/arguments/set_argument.py
@@ -8,7 +8,6 @@ from gitflow_wotw.components import ArgumentInstance
 class SetArgument(ArgumentInstance):
     args = ['--set']
     kwargs = {
-        'nargs': 1,
         'dest': 'set',
         'metavar': 'BASE_BRANCH',
         'help': 'Use the provided branch as the base'
