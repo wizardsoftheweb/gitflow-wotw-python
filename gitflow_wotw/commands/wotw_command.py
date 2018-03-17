b0397583e98234681274ac675de9049f6b022ada
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 06:19:46 -0500

    Create specific WotwCommand

---
+++ gitflow_wotw/commands/wotw_command.py
@@ -0,0 +1,35 @@
+# pylint: disable=W,C,R
+
+from gitflow_wotw.actions import InitAction, VersionAction, LogAction
+from gitflow_wotw.components import Command
+from gitflow_wotw.subcommands import (
+    FeatureSubcommand,
+    BugfixSubcommand,
+    ReleaseSubcommand,
+    HotfixSubcommand,
+    SupportSubcommand,
+    ConfigSubcommand
+)
+
+
+class WotwCommand(Command):
+    IDENTIFIER = 'wotw'
+    HELP_STRING = 'An ambitious attempt to duplicate gitflow-avh in Python'
+
+    def __init__(self):
+        WotwCommand.__init__(
+            self,
+            self.IDENTIFIER,
+            self.HELP_STRING
+        )
+
+    def popuplate(self):
+        self['init'] = InitAction()
+        self['feature'] = FeatureSubcommand()
+        self['bugfix'] = BugfixSubcommand()
+        self['release'] = ReleaseSubcommand()
+        self['hotfix'] = HotfixSubcommand()
+        self['support'] = SupportSubcommand()
+        self['version'] = VersionAction()
+        self['config'] = ConfigSubcommand()
+        self['log'] = LogAction()
