e351a312d396b1d758e7ca136f94549acb2bcb0d
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 14:17:09 -0500

    Update to include branch and base positionals

--- gitflow_wotw/actions/base_action.py
+++ gitflow_wotw/actions/base_action.py
@@ -15,7 +15,7 @@ from gitflow_wotw.components import ActionInstance, ArgumentGroup
 
 class BaseAction(ActionInstance):
     identifier = 'base'
-    help_string = 'Set base config'
+    help_string = 'Set base branch'
 
     def __init__(self):
         ActionInstance.__init__(self)
@@ -30,9 +30,34 @@ class BaseAction(ActionInstance):
         )
 
     def execute(self, parsed):
-        print('Firing base!')
-        self.parse_config_location(parsed)
+        print(parsed)
+        if not parsed.branch:
+            if parsed.set:
+                return self.set_base(None, parsed.set)
+            return self.get_base()
+        else:
+            if not parsed.base:
+                if parsed.set:
+                    return self.set_base(parsed.branch, parsed.set)
+            else:
+                if parsed.set:
+                    if parsed.set == parsed.base:
+                        return self.set_base(parsed.branch, parsed.set)
+                    else:
+                        raise ValueError(
+                            '--set and base are different;'
+                            ' remove one and try again'
+                        )
+                else:
+                    return self.set_base(parsed.branch, parsed.base)
+            return self.get_base(parsed.branch)
 
     @staticmethod
     def parse_config_location(parsed):
         print(parsed)
+
+    def get_base(self, branch=None):
+        self.flow_branch.base_branch(branch)
+
+    def set_base(self, branch=None, base=None):
+        self.flow_branch.update_base(branch, base)
