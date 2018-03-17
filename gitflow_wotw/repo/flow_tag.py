9a47dfbdc06cae4d355d3cd5ee23b772e9d6103b
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 10:55:20 -0500

    Refactor with slimmer FlowTag interface

--- gitflow_wotw/repo/flow_tag.py
+++ gitflow_wotw/repo/flow_tag.py
@@ -17,35 +17,27 @@ class FlowTag(HasConfig):
         super(FlowTag, self).__init__(directory, config)
         self.tags = OrderedDict()
         self.versions = OrderedDict()
-        print(self.repo.references.get('refs/tags/'))
-        # self.parse_references()
+        self.parse_tags()
 
     @property
     def version_tag(self):
-        return self.config['gitflow.prefix.versiontag']
+        return (
+            'gitflow.prefix.versiontag' in self.config
+            and
+            self.config['gitflow.prefix.versiontag'].encode('utf-8')
+        )
 
-    def parse_references(self):
-        for reference in self.repo.references:
-            tag, tag_id = self.check_for_tag(reference)
-            if tag or tag_id:
-                self.check_for_version_tag(tag, tag_id)
-
-    def check_for_tag(self, reference):
-        if reference.startswith('refs/tags/'):
+    def parse_tags(self):
+        for reference in [
+                reference
+                for reference in self.repo.references
+                if reference.startswith('refs/tags/')
+        ]:
             tag = reference.replace('refs/tags/', '')
             tag_id = self.repo.lookup_reference(reference).peel().id
+            if self.version_tag and tag.startswith(self.version_tag):
+                self.versions[tag_id] = tag.replace(self.version_tag, '', 1)
             self.tags[tag_id] = tag
-            return tag, tag_id
-        return None, None
-
-    def check_for_version_tag(self, tag=None, tag_id=None):
-        if not self.config.version_tag is None:
-            if tag.startswith(self.config.version_tag):
-                self.versions[tag_id] = tag.replace(
-                    self.config.version_tag,
-                    '',
-                    1
-                )
 
     def walk_for_tags(self):
         last = None
@@ -58,15 +50,3 @@ class FlowTag(HasConfig):
             if last and version:
                 break
         return last, version
-
-    def important_refs(self):
-        # flw, branch = self.active_flow_and_branch()
-        # base = self.base_from_branch(branch)
-        tag, version = self.walk_for_tags()
-        # return OrderedDict({
-        #     'floww': floww,
-        #     'branch': branch,
-        #     'base': base,
-        #     'tag': tag,
-        #     'version': version
-        # })
