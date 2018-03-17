aa6e2225b40c6e9179bf2bde4aaffd904a3c47b9
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 03:15:55 -0500

    Attach UniversalArgumentGroup to base Action

--- gitflow_wotw/components/base_action.py
+++ gitflow_wotw/components/base_action.py
@@ -1,9 +1,14 @@
 # pylint:disable=W,C,R
 
+from gitflow_wotw.arguments.groups import UniversalArgumentGroup
 from gitflow_wotw.parsers import ParserSink
 
 
 class Action(ParserSink):
+
+    def __init__(self, *args, **kwargs):
+        ParserSink.__init__(self, *args, **kwargs)
+        self.arguments['universal'] = UniversalArgumentGroup()
 
     def execute(self, parsed):
         """A specific task to be defined by instances"""
