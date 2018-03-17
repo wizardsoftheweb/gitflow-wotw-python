8c239a68348b3c26b59315ac12e2822b0a7e00dd
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 12:10:21 -0500

    Refactor SubcommandInstance with ComponentInstance

--- gitflow_wotw/components/subcommand_instance.py
+++ gitflow_wotw/components/subcommand_instance.py
@@ -2,24 +2,15 @@
 
 from __future__ import print_function
 
-from gitflow_wotw.components import Subcommand
+from gitflow_wotw.components import ComponentInstance, Subcommand
 
 
-class SubcommandInstance(Subcommand):
-    SUBCOMMAND = ''
-    HELP_STRING = ''
-    ACTIONS = []
+class SubcommandInstance(ComponentInstance, Subcommand):
 
     def __init__(self):
-        super(
-            SubcommandInstance,
-            self
-        ).__init__(
-            self.SUBCOMMAND,
-            self.HELP_STRING
+        ComponentInstance.__init__(self)
+        Subcommand.__init__(
+            self,
+            self.identifier,
+            self.help_string
         )
-        self.populate()
-
-    def populate(self):
-        for action in self.ACTIONS:
-            self[action.identifier] = action
