aed28f668bb74feeab5f4367e03b57631de370b9
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-16 22:24:08 -0500

    Export BranchSubcommand

--- gitflow_wotw/components/__init__.py
+++ gitflow_wotw/components/__init__.py
@@ -3,4 +3,5 @@
 from .base_argument import Argument
 from .base_action import Action
 from .base_subcommand import Subcommand
+from .branch_subcommand import BranchSubcommand
 from .command import Command
