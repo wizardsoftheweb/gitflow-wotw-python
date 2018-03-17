98deef4409baaa7521c77e4a1acd897b06db24eb
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 12:39:20 -0500

    Shift BranchSubcommand down in export order

--- gitflow_wotw/components/__init__.py
+++ gitflow_wotw/components/__init__.py
@@ -4,7 +4,6 @@ from .base_argument import Argument
 from .argument_group import ArgumentGroup
 from .base_action import Action
 from .base_subcommand import Subcommand
-from .branch_subcommand import BranchSubcommand
 from .command import Command
 from .component_instance import ComponentInstance
 from .argument_instance import ArgumentInstance
@@ -12,3 +11,4 @@ from .argument_group_instance import ArgumentGroupInstance
 from .action_instance import ActionInstance
 from .subcommand_instance import SubcommandInstance
 from .command_instance import CommandInstance
+from .branch_subcommand import BranchSubcommand
