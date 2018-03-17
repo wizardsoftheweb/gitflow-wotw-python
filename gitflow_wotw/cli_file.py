fa62653647389a8c28240c96e6d312ac741da6bc
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 01:19:49 -0500

    Update CLI definition

--- gitflow_wotw/cli_file.py
+++ gitflow_wotw/cli_file.py
@@ -2,10 +2,26 @@
 
 from gitflow_wotw.components import Command
 
-from gitflow_wotw.subcommands import FeatureSubcommand
+from gitflow_wotw.actions import InitAction, VersionAction, LogAction
+from gitflow_wotw.subcommands import (
+    FeatureSubcommand,
+    BugfixSubcommand,
+    ReleaseSubcommand,
+    HotfixSubcommand,
+    SupportSubcommand,
+    ConfigSubcommand
+)
 
 
 def cli():
     demo = Command()
+    demo['init'] = InitAction()
     demo['feature'] = FeatureSubcommand()
+    demo['bugfix'] = BugfixSubcommand()
+    demo['release'] = ReleaseSubcommand()
+    demo['hotfix'] = HotfixSubcommand()
+    demo['support'] = SupportSubcommand()
+    demo['version'] = VersionAction()
+    demo['config'] = ConfigSubcommand()
+    demo['log'] = LogAction()
     demo.bootstrap()
