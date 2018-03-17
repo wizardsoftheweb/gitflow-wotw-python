c7fdc9ec703fdc601f1999c761446201b0232def
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 15:49:08 -0500

    Add branch and base arguments

--- gitflow_wotw/actions/diff_action.py
+++ gitflow_wotw/actions/diff_action.py
@@ -2,9 +2,22 @@
 
 from __future__ import print_function
 
+from gitflow_wotw.arguments import BaseArgument, BranchArgument
 from gitflow_wotw.components import ActionInstance
 
 
 class DiffAction(ActionInstance):
     identifier = 'diff'
     help_string = 'Compare the branch against its base'
+
+    def __init__(self):
+        ActionInstance.__init__(self)
+        self.arguments['branch'] = BranchArgument()
+        self.arguments['base'] = BaseArgument()
+
+    def execute(self, parsed):
+        print(parsed)
+        print(self.flow_branch.branch_to_commit_id())
+        print(self.flow_branch.branch_to_commit_id(
+            self.flow_branch.base_branch()
+        ))
