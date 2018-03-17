a3fa09a01844bf2e5373e66ac4a727e0dee8cc70
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 12:08:41 -0500

    Refactor ArgumentGroupInstance with ComponentInstance

--- gitflow_wotw/components/argument_group_instance.py
+++ gitflow_wotw/components/argument_group_instance.py
@@ -4,19 +4,20 @@ from __future__ import print_function
 
 from collections import OrderedDict
 
-from gitflow_wotw.components import ArgumentGroup
+from gitflow_wotw.components import ArgumentGroup, ComponentInstance
 
 
-class ArgumentGroupInstance(ArgumentGroup):
-    SEED = OrderedDict()
-    TITLE = None
-    DESCRIPTION = None
-    EXCLUSIVE = False
+class ArgumentGroupInstance(ComponentInstance, ArgumentGroup):
+    seed = OrderedDict()
+    title = None
+    help_string = None
+    exclusive = False
 
     def __init__(self):
+        ComponentInstance.__init__(self)
         super(ArgumentGroupInstance, self).__init__(
-            self.SEED,
-            self.TITLE,
-            self.DESCRIPTION,
-            self.EXCLUSIVE
+            seed=self.seed,
+            title=self.title,
+            description=self.help_string,
+            exclusive=self.exclusive
         )
