e258b30d73dcc43f3eee3421d4cab48c5d35184b
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 18:13:29 -0500

    Add pre and post action methods

--- gitflow_wotw/components/action_instance.py
+++ gitflow_wotw/components/action_instance.py
@@ -9,6 +9,8 @@ from gitflow_wotw.components import Action, ComponentInstance
 
 
 class ActionInstance(ComponentInstance, Action):
+    pre_execution = OrderedDict()
+    post_execution = OrderedDict()
 
     def __init__(self):
         ComponentInstance.__init__(self)
@@ -18,7 +20,16 @@ class ActionInstance(ComponentInstance, Action):
             self.help_string
         )
         self.arguments['universal'] = UniversalArgumentGroup()
+        self.pre_execution['tidy_branches'] = []
+
+    def pre_execute(self, parsed):
+        for handler, args in self.pre_execution.items():
+            self.handlers[handler](self, parsed, *args)
 
     def execute(self, parsed):
         print("Firing %s!" % self.identifier)
         print(parsed)
+
+    def post_execute(self, parsed):
+        for handler, args in self.post_execution.items():
+            self.handlers[handler](self, parsed, *args)
