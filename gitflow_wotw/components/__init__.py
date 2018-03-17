21dca3148d2245866920cc5323685a26ab5d78c7
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 11:37:57 -0500

    Export ArgumentInstance

--- gitflow_wotw/components/__init__.py
+++ gitflow_wotw/components/__init__.py
@@ -7,3 +7,4 @@ from .base_subcommand import Subcommand
 from .branch_subcommand import BranchSubcommand
 from .command import Command
 from .component_instance import ComponentInstance
+from .argument_instance import ArgumentInstance
