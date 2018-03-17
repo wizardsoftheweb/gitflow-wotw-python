3902729ebb633552d2f194840d89e05ef9806def
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 08:39:17 -0500

    Create HasConfig

---
+++ gitflow_wotw/repo/has_config.py
@@ -0,0 +1,20 @@
+# pylint: disable=W,C,R
+
+from __future__ import print_function
+
+from collections import OrderedDict
+
+from pygit2 import Config
+
+from gitflow_wotw.repo import GitConfig
+from gitflow_wotw.utils import HasRepository
+
+
+class HasConfig(HasRepository):
+
+    def __init__(self, directory=None, config=None):
+        super(HasConfig, self).__init__(directory)
+        if isinstance(config, GitConfig):
+            self.config = config
+        else:
+            self.config = GitConfig(directory)
