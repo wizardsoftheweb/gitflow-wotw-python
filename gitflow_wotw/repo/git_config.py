0298ac71343be789baa778a3914de00c24464b30
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 04:11:50 -0500

    Finish drafting GitConfig

--- gitflow_wotw/repo/git_config.py
+++ gitflow_wotw/repo/git_config.py
@@ -2,7 +2,9 @@
 
 from __future__ import print_function
 
-from pygit2 import config
+from collections import OrderedDict
+
+from pygit2 import Config
 
 from gitflow_wotw.utils import HasRepository
 
@@ -11,4 +13,33 @@ class GitConfig(HasRepository):
 
     def __init__(self, directory=None):
         super(GitConfig, self).__init__(directory)
-        print(self.repo.config)
+        self.load_and_sort()
+
+    def load_repo_config(self):
+        config = {}
+        for key in self.repo.config:
+            config[key] = self.repo.config[key]
+        return config
+
+    def sort_config(self, unsorted_config=None):
+        config = OrderedDict()
+        for key in sorted(unsorted_config.iterkeys()):
+            config[key] = unsorted_config[key]
+        return config
+
+    def load_and_sort(self):
+        raw = self.load_repo_config()
+        self.config = self.sort_config(raw)
+
+    @staticmethod
+    def dump_config(config=None):
+        for key, value in config.items():
+            print(key, value)
+
+    @staticmethod
+    def global_config():
+        return Config.get_global_config()
+
+    @staticmethod
+    def system_config():
+        return Config.get_system_config()
