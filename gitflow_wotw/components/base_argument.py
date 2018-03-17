2859e5825d234ca57ab671ea55528b21eefbcf1d
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 01:41:07 -0500

    Update Argument to negate boolean arguments

--- gitflow_wotw/components/base_argument.py
+++ gitflow_wotw/components/base_argument.py
@@ -11,3 +11,18 @@ class Argument(object):
 
     def attach_argument(self, parser=None):
         parser.add_argument(*self.args, **self.kwargs)
+        if 'action' in self.kwargs and 'store_true' == self.kwargs['action']:
+            self.negate(parser)
+
+    def negate(self, parser=None):
+        negated_args = []
+        for arg in self.args:
+            if arg.startswith('--'):
+                negated_args.append(arg.replace('--', '--no-'))
+        if negated_args:
+            negated_kwargs = self.kwargs
+            negated_kwargs['help'] = (
+                "Do not %s" % negated_kwargs['help'].lower()
+            )
+            negated_kwargs['action'] = 'store_false'
+        parser.add_argument(*negated_args, **negated_kwargs)
