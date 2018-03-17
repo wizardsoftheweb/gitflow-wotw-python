946c4f66804b593fbe45650ff7a2f71c353b803c
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 06:17:45 -0500

    Remove specifics from components.Command

--- gitflow_wotw/components/command.py
+++ gitflow_wotw/components/command.py
@@ -2,28 +2,24 @@
 
 from collections import OrderedDict
 
-
 from gitflow_wotw.parsers import ParserSource
 from gitflow_wotw.constants import HIERARCHY
 from gitflow_wotw.components import Subcommand
 
 
-# class Command(SourceParser, HasDescendants):
 class Command(ParserSource, Subcommand):
-    IDENTIFIER = 'wotw'
-    HELP_STRING = 'An ambitious attempt to duplicate gitflow-avh in Python'
 
-    def __init__(self):
+    def __init__(self, identifier=None, help_string=None,):
         Subcommand.__init__(
             self,
-            self.IDENTIFIER,
-            self.HELP_STRING,
+            identifier,
+            help_string,
             HIERARCHY[0]
         )
         ParserSource.__init__(
             self,
-            self.IDENTIFIER,
-            self.HELP_STRING,
+            identifier,
+            help_string,
             HIERARCHY[0]
         )
 
