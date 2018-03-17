7e30641b3435e2585e339b02e293a8d87d56b62e
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 00:46:12 -0500

    Add DiffAction arguments

--- gitflow_wotw/actions/diff_action.py
+++ gitflow_wotw/actions/diff_action.py
@@ -2,19 +2,20 @@
 
 from __future__ import print_function
 
+from gitflow_wotw.arguments import ShowCommandsArgument
 from gitflow_wotw.components import Action
 
 
 class DiffAction(Action):
     ACTION = 'diff'
-    HELP_STRING = 'Compares the diff of somethings'
+    HELP_STRING = 'Compare the branch against its base'
 
     def __init__(self):
         super(DiffAction, self).__init__(self.ACTION, self.HELP_STRING)
         self.populate()
 
     def populate(self):
-        """"""
+        self.arguments['show_commands'] = ShowCommandsArgument()
 
     def execute(self, parsed):
         print('Firing diff!')
