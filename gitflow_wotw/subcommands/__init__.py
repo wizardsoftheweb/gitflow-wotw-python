bbd28ed93c58108f3cbd6a53b3a3737282fbcfbd
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-16 22:08:27 -0500

    Export Subcommands

--- gitflow_wotw/subcommands/__init__.py
+++ gitflow_wotw/subcommands/__init__.py
@@ -1,3 +1,11 @@
 # pylint: disable=W,C,R
 
+from .init_subcommand import InitSubcommand
 from .feature_subcommand import FeatureSubcommand
+from .bugfix_subcommand import BugfixSubcommand
+from .release_subcommand import ReleaseSubcommand
+from .hotfix_subcommand import HotfixSubcommand
+from .support_subcommand import SupportSubcommand
+from .version_subcommand import VersionSubcommand
+from .config_subcommand import ConfigSubcommand
+from .log_subcommand import LogSubcommand
