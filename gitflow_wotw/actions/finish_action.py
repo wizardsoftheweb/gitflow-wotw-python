b87f430d751468f076a01bd6c820b1fde719a0ea
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 00:53:44 -0500

    Add FinishAction arguments

--- gitflow_wotw/actions/finish_action.py
+++ gitflow_wotw/actions/finish_action.py
@@ -2,6 +2,25 @@
 
 from __future__ import print_function
 
+from gitflow_wotw.arguments import (
+    DeleteArgument,
+    DeleteLocalArgument,
+    DeleteRemoteArgument,
+    FetchArgument,
+    FfArgument,
+    ForceArgument,
+    KeepArgument,
+    KeepLocalArgument,
+    KeepRemoteArgument,
+    PushArgument,
+    PushDevelopArgument,
+    PushProductionArgument,
+    PushTagArgument,
+    PreserveMergesArgument,
+    RebaseArgument,
+    ShowCommandsArgument,
+    SquashArgument
+)
 from gitflow_wotw.components import Action
 
 
@@ -14,7 +33,23 @@ class FinishAction(Action):
         self.populate()
 
     def populate(self):
-        """"""
+        self.arguments['fetch'] = FetchArgument()
+        self.arguments['rebase'] = RebaseArgument()
+        self.arguments['preserve_merges'] = PreserveMergesArgument()
+        self.arguments['delete'] = DeleteArgument()
+        self.arguments['delete_local'] = DeleteLocalArgument()
+        self.arguments['delete_remote'] = DeleteRemoteArgument()
+        self.arguments['keep'] = KeepArgument()
+        self.arguments['keep_local'] = KeepLocalArgument()
+        self.arguments['keep_remote'] = KeepRemoteArgument()
+        self.arguments['push'] = PushArgument()
+        self.arguments['push_develop'] = PushDevelopArgument()
+        self.arguments['push_master'] = PushProductionArgument()
+        self.arguments['push_tag'] = PushTagArgument()
+        self.arguments['force'] = ForceArgument()
+        self.arguments['squash'] = SquashArgument()
+        self.arguments['ff'] = FfArgument()
+        self.arguments['show_commands'] = ShowCommandsArgument()
 
     def execute(self, parsed):
         print('Firing finish!')
