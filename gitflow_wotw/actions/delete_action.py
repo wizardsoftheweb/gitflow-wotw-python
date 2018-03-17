42b9cfd559cafdbcd9487e9fa59f861512db83ef
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 15:36:06 -0500

    Finish execute and add BranchArgument

--- gitflow_wotw/actions/delete_action.py
+++ gitflow_wotw/actions/delete_action.py
@@ -2,6 +2,7 @@
 
 from __future__ import print_function
 
+from gitflow_wotw.arguments import BranchArgument
 from gitflow_wotw.arguments.groups import BranchArgumentGroup
 from gitflow_wotw.components import ActionInstance
 
@@ -12,4 +13,30 @@ class DeleteAction(ActionInstance):
 
     def __init__(self):
         ActionInstance.__init__(self)
+        self.arguments['branch'] = BranchArgument()
         self.arguments['branches'] = BranchArgumentGroup()
+
+    def execute(self, parsed):
+        if not parsed.branch:
+            branch = self.flow_branch.branch
+        upstream = self.flow_branch.upstream_branch(branch)
+        self.flow_branch.change_to_base_branch(branch)
+        if 'local' == parsed.keep:
+            self.flow_branch.delete_remote_branch(
+                upstream,
+                parsed.force
+            )
+        elif 'remote' == parsed.keep:
+            self.flow_branch.delete_local_branch(
+                branch,
+                parsed.force
+            )
+        elif not parsed.keep or 'both' == parsed.delete:
+            self.flow_branch.delete_remote_branch(
+                upstream,
+                parsed.force
+            )
+            self.flow_branch.delete_local_branch(
+                branch,
+                parsed.force
+            )
