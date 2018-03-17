6eab32fc9da796b37481425066db21f0bf467bed
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 00:45:42 -0500

    Add DeleteAction arguments

--- gitflow_wotw/actions/delete_action.py
+++ gitflow_wotw/actions/delete_action.py
@@ -2,6 +2,16 @@
 
 from __future__ import print_function
 
+from gitflow_wotw.arguments import (
+    DeleteArgument,
+    DeleteLocalArgument,
+    DeleteRemoteArgument,
+    KeepArgument,
+    KeepLocalArgument,
+    KeepRemoteArgument,
+    ForceArgument,
+    ShowCommandsArgument
+)
 from gitflow_wotw.components import Action
 
 
@@ -14,7 +24,14 @@ class DeleteAction(Action):
         self.populate()
 
     def populate(self):
-        """"""
+        self.arguments['delete'] = DeleteArgument()
+        self.arguments['delete_local'] = DeleteLocalArgument()
+        self.arguments['delete_remote'] = DeleteRemoteArgument()
+        self.arguments['keep'] = KeepArgument()
+        self.arguments['keep_local'] = KeepLocalArgument()
+        self.arguments['keep_remote'] = KeepRemoteArgument()
+        self.arguments['force'] = ForceArgument()
+        self.arguments['show_commands'] = ShowCommandsArgument()
 
     def execute(self, parsed):
         print('Firing delete!')
