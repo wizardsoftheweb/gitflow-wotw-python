ec088dfba8510a3e4a2653e4298b5e884656cd17
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 12:16:57 -0500

    Refactor DeleteAction

--- gitflow_wotw/actions/delete_action.py
+++ gitflow_wotw/actions/delete_action.py
@@ -3,20 +3,13 @@
 from __future__ import print_function
 
 from gitflow_wotw.arguments.groups import BranchArgumentGroup
-from gitflow_wotw.components import Action, ArgumentGroup
+from gitflow_wotw.components import ActionInstance
 
 
-class DeleteAction(Action):
-    ACTION = 'delete'
-    HELP_STRING = 'Delete a specific branch'
+class DeleteAction(ActionInstance):
+    identifier = 'delete'
+    help_string = 'Delete a specific branch'
 
     def __init__(self):
-        super(DeleteAction, self).__init__(self.ACTION, self.HELP_STRING)
-        self.populate()
-
-    def populate(self):
+        super(DeleteAction, self).__init__(self)
         self.arguments['branches'] = BranchArgumentGroup()
-
-    def execute(self, parsed):
-        print('Firing delete!')
-        print(parsed)
