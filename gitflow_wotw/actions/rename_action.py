914834eec80f7439dceca028900373385d1372bf
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 01:02:03 -0500

    Add RenameAction arguments

--- gitflow_wotw/actions/rename_action.py
+++ gitflow_wotw/actions/rename_action.py
@@ -2,6 +2,7 @@
 
 from __future__ import print_function
 
+from gitflow_wotw.arguments import ShowCommandsArgument
 from gitflow_wotw.components import Action
 
 
@@ -14,7 +15,7 @@ class RenameAction(Action):
         self.populate()
 
     def populate(self):
-        """"""
+        self.arguments['show_commands'] = ShowCommandsArgument()
 
     def execute(self, parsed):
         print('Firing rename!')
