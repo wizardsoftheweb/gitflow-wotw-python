b9c15a55e0e0bbd6bb5e6ac94631d3abb8ddf222
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-18 13:05:25 -0500

    Log Action items

--- gitflow_wotw/components/action.py
+++ gitflow_wotw/components/action.py
@@ -4,12 +4,16 @@ from __future__ import print_function
 
 from argparse import ArgumentParser
 from collections import OrderedDict
+from logging import getLogger
 from sys import argv
+
+LOGGER = getLogger(__name__)
 
 
 class Action(object):
 
     def __init__(self, identifier=None, help_string=None):
+        LOGGER.debug("Initialized a %s Action")
         self.identifier = identifier
         self.help_string = help_string
         self.arguments = []
@@ -18,6 +22,7 @@ class Action(object):
         """"""
 
     def attach_as_subcommand(self, subparsers):
+        LOGGER.info("Attaching %s as a subcommand", self.identifier)
         self.parser = subparsers.add_parser(
             self.identifier,
             add_help=False,
@@ -26,10 +31,12 @@ class Action(object):
         )
 
     def attach_arguments(self, subparsers):
+        LOGGER.info("Attaching arguments for %s", self.identifier)
         self.attach_as_subcommand(subparsers)
         for argument in self.arguments:
             argument.attach_arguments(self.parser)
 
     def process(self, parsed=None, args=None):
+        LOGGER.warning("Base process used for %s Action", self.identifier)
         print(parsed)
         print(args)
