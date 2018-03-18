25e91e3010bf2e3827a5a9ffdd322c58aadfc5e7
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 23:19:06 -0500

    Simplify ctor

--- gitflow_wotw/repo/has_config.py
+++ gitflow_wotw/repo/has_config.py
@@ -11,7 +11,7 @@ from gitflow_wotw.utils import HasRepository
 class HasConfig(HasRepository):
 
     def __init__(self, directory=None, config=None):
-        super(HasConfig, self).__init__(directory)
+        HasRepository.__init__(self, directory, config)
         if isinstance(config, GitConfig):
             self.config = config
         else:
