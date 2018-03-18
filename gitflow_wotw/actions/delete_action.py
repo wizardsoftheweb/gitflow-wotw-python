84aea011fc0940d4af9b64cb8fd4d13ac6263463
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 19:04:13 -0500

    Clean up old comments

--- gitflow_wotw/actions/delete_action.py
+++ gitflow_wotw/actions/delete_action.py
@@ -16,28 +16,3 @@ class DeleteAction(ActionInstance):
         self.arguments['branch'] = BranchArgument()
         self.arguments['branches'] = BranchArgumentGroup()
         self.post_execution['delete_branch'] = []
-
-    # def execute(self, parsed):
-    #     if not parsed.branch:
-    #         branch = self.flow_branch.branch
-    #     upstream = self.flow_branch.upstream_branch(branch)
-    #     self.flow_branch.change_to_base_branch(branch)
-    #     if 'local' == parsed.keep:
-    #         self.flow_branch.delete_remote_branch(
-    #             upstream,
-    #             parsed.force
-    #         )
-    #     elif 'remote' == parsed.keep:
-    #         self.flow_branch.delete_local_branch(
-    #             branch,
-    #             parsed.force
-    #         )
-    #     elif not parsed.keep or 'both' == parsed.delete:
-    #         self.flow_branch.delete_remote_branch(
-    #             upstream,
-    #             parsed.force
-    #         )
-    #         self.flow_branch.delete_local_branch(
-    #             branch,
-    #             parsed.force
-    #         )
