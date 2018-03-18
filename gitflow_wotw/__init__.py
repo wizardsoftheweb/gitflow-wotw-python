c977b040cbad77b646deb48943c048ff23f8c188
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-18 12:54:34 -0500

    Add logging

--- gitflow_wotw/__init__.py
+++ gitflow_wotw/__init__.py
@@ -12,3 +12,15 @@ from .generators import (
     ObjectBuilder,
 )
 from .cli_file import cli
+
+from logging import debug, info
+from logging.config import dictConfig
+from os.path import abspath, dirname, join
+
+__location__ = abspath(dirname(__file__))
+
+from yaml import load
+
+with open(join(__location__, 'logging.yml'), 'r') as config_file:
+    config = load(config_file)
+dictConfig(config)
