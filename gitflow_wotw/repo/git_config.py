d21707298c5a2bcdfaf42e9d751c161550ee7ab0
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 19:43:11 -0500

    Refactor GitConfig

--- gitflow_wotw/repo/git_config.py
+++ gitflow_wotw/repo/git_config.py
@@ -3,50 +3,52 @@
 from __future__ import print_function
 
 from collections import OrderedDict
-
-from pygit2 import Config
+from subprocess import check_output
 
 from gitflow_wotw.utils import HasRepository
+from re import compile as re_compile, match, MULTILINE, VERBOSE
 
 
 class GitConfig(HasRepository, OrderedDict):
+    CONFIG_LINE_PATTERN = re_compile(
+        r"""
+        ^\s*(?P<key>.*?)=(?P<value>.*?)\s*$
+        """,
+        MULTILINE | VERBOSE
+    )
 
     def __init__(self, directory=None):
         OrderedDict.__init__(self)
         HasRepository.__init__(self, directory)
-        self.prefixes = {}
         self.load_and_sort()
-        if 'versiontag' in self.prefixes:
-            self.version_tag = self.prefixes['versiontag']
-        else:
-            self.version_tag = None
 
-    def load_repo_config(self):
-        config = {}
-        for key in self.repo.config:
-            config[key] = self.repo.config[key]
-        return config
+    @staticmethod
+    def get_active_config():
+        return check_output([
+            'git',
+            'config',
+            '--list'
+        ]).strip().split('\n')
 
-    def sort_config(self, unsorted_config=None):
-        for key in sorted(unsorted_config.iterkeys()):
-            self[key] = unsorted_config[key]
-            if key.startswith('gitflow.prefix.'):
-                flow = key.replace('gitflow.prefix.', '')
-                self.prefixes[flow] = self[key]
+    def parse_config(self, config_to_parse):
+        for row in config_to_parse:
+            matched = match(GitConfig.CONFIG_LINE_PATTERN, row)
+            if matched:
+                key = matched.group('key')
+                value = matched.group('value')
+                if key in self:
+                    if isinstance(self[key], list):
+                        self[key] = self[key] + [value]
+                    else:
+                        self[key] = [self[key], value]
+                else:
+                    self[key] = value
 
     def load_and_sort(self):
-        raw = self.load_repo_config()
-        self.sort_config(raw)
+        raw = self.get_active_config()
+        self.parse_config(raw)
 
     @staticmethod
     def dump_config(config=None):
         for key, value in config.items():
             print(key, value)
-
-    @staticmethod
-    def global_config():
-        return Config.get_global_config()
-
-    @staticmethod
-    def system_config():
-        return Config.get_system_config()
