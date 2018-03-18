bf6c8ffd76671872412dd93081726f96cf6b55f1
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-17 21:11:23 -0500

    Export branch decorator

--- gitflow_wotw/utils/__init__.py
+++ gitflow_wotw/utils/__init__.py
@@ -3,3 +3,4 @@
 from .observes_hierarchy import ObservesHierarchy
 from .has_descendants import HasDescendants
 from .has_repository import HasRepository
+from .branch_decorator import branch_decorator
