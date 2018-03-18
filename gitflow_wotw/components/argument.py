751cbb30db1e85c59489d7bed640ea20b5a8885b
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-18 13:07:09 -0500

    Log Argument actions

--- gitflow_wotw/components/argument.py
+++ gitflow_wotw/components/argument.py
@@ -2,12 +2,19 @@
 
 from __future__ import print_function
 
+from logging import getLogger
+
+LOGGER = getLogger(__name__)
+
 
 class Argument(object):
 
     def __init__(self, *args, **kwargs):
+        LOGGER.debug('Initialized an Argument')
+        LOGGER.debug(*args)
         self.args = args
         self.kwargs = kwargs
 
     def attach_arguments(self, parser=None):
+        LOGGER.info('Attaching arguments')
         parser.add_argument(*self.args, **self.kwargs)
